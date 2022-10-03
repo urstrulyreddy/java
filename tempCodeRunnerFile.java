b5d67f52baa63544732586cbaca6583a20a869ef
Deependra <deependrareddy@Deependras-MacBook-Air.local>
2022-10-03 21:24:09 +0530

    First commit

---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,9 @@
+public class main
+{
+    public static void main(string[]args)
+    {
+        string name="not empty";
+        system.out.printin(name.isempty());
+        system.out.printin("hello mac");
+    }
+}
