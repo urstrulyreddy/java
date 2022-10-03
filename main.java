888fa73d8f7e1d753126b8363123176095b5f9eb
urstrulyreddy <96733257+urstrulyreddy@users.noreply.github.com>
2022-10-03 22:24:08 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args) {
+        String name = "";
+        System.out.println(name.isEmpty());
+        System.out.println("hello mac");
+    }
+}
