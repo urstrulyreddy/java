a21af7fe25fd172dcf36ea8cc8e0cdc7ff64a869
Deependra <deependrareddy@Deependras-MacBook-Air.local>
2022-10-06 14:34:59 +0530

    Changes in commit

--- main.java
+++ main.java
@@ -2,6 +2,7 @@ public class main {
     public static void main(String[] args) {
         String name = "";
         System.out.println(name.isEmpty());
+
         System.out.println("hello mac");
     }
 }
